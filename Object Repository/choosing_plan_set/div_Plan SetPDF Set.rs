a014553ff462d3b461221a62b80d2034d96a39c5
henry <hhy@hanleywood.com>
2019-04-25 16:24:15 -0700

    select PDF set

---
+++ Object Repository/choosing_plan_set/div_Plan SetPDF Set.rs
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Plan SetPDF Set</name>
+   <tag></tag>
+   <elementGuidId>e59bc66d-1983-43de-a440-279d1ed647fa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Plan #430-165'])[1]/following::div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>h6 light-font my-0</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Plan Set:  PDF Set</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;container-fluid p-0&quot;]/div[@class=&quot;container-fluid max-width-lg&quot;]/section[@class=&quot;mt-4 mx-0 mx-md-2 pb-3 cart&quot;]/div[@class=&quot;row no-gutters&quot;]/div[@class=&quot;col-12 px-2 mb-4&quot;]/ul[@class=&quot;list-unstyled&quot;]/li[@class=&quot;w-100 mb-3 position-relative&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12 col-md-6 mx-auto pt-2&quot;]/div[@class=&quot;h6 light-font my-0&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Plan #430-165'])[1]/following::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
